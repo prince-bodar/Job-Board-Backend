70c6eba52fd6ed90a12b915494821b15f3e3c6ab
prince-bodar <bodarprins4@gmail.com>
2025-05-15 18:03:48 +0530

    URL change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://job-board-backend-2y2z.onrender.com',
+
+  baseURL: "https://job-board-backend-2y2z.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
